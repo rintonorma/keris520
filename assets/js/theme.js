7add017e1d2d509a4f728238342011c30e325d19
rintonorma <rinto456@gmail.com>
2022-04-04 22:54:14 +0700

    be a talent

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -589,5 +589,42 @@
     }
   });
 
+  // ====================
+  // Dropdown Country
+  // ====================
+
+  function format(item, state) {
+    if (!item.id) {
+      return item.text;
+    }
+    var countryUrl = "https://hatscripts.github.io/circle-flags/flags/";
+    var stateUrl = "https://oxguy3.github.io/flags/svg/us/";
+    var url = state ? stateUrl : countryUrl;
+    var img = $("<img>", {
+      class: "img-flag",
+      width: 26,
+      src: url + item.element.value.toLowerCase() + ".svg"
+    });
+    var span = $("<span>", {
+      text: " " + item.text
+    });
+    span.prepend(img);
+    return span;
+  }
+
+  $(document).ready(function() {
+    $("#countries").select2({
+      templateResult: function(item) {
+        return format(item, false);
+      }
+    });
+    $("#us-states").select2({
+      templateResult: function(item) {
+        return format(item, true);
+      }
+    });
+  });
+
+
 
 })(jQuery); 
